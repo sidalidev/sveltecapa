10b3c7a55ccb65028bb7692b73a62f2e23bba3e8
Sid Ali Bentifraouine <sidbentifraouine@gmail.com>
2022-06-20 11:23:33 +0200

    config: ios & android

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<main>
+	<slot />
+</main>
+
+<style lang="postcss">
+	main {
+		/* @apply pt-4; */
+		padding-top: 30px;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Dialog } from '@capacitor/dialog';
+	import { Toast } from '@capacitor/toast';
+
+	async function show() {
+		// await Dialog.alert({
+		// 	title: 'Stop',
+		// 	message: 'this is an error'
+		// });
+		await Toast.show({
+			text: 'Hello!'
+		});
+	}
+</script>
+
+<button on:click={show}>Show</button>
